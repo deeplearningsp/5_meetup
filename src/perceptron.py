63d0a5ef4762995800cf163e5fe48da7184cd1cd
Ivan <ispmarin@localhost.localdomain>
2016-09-12 22:47:47 -0300

    remove sklearn dep

--- src/perceptron.py
+++ src/perceptron.py
@@ -1,29 +1,29 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error
+
 
 class Perceptron:
     
-    def __init__(self, num_inputs, activation_threshold, target):
+    def __init__(self, num_inputs, activation_threshold, target, learn_const):
         self.num_inputs = num_inputs
         self.activation_threshold = activation_threshold
         self.target = target
+        self.learn_const = learn_const
         self.inputs = np.zeros(self.num_inputs)
         self.weights = np.random.rand(self.num_inputs)
 
     def feed_forward(self, inputs):
         return np.sum(inputs * self.weights)
     
-    def activate_function(self, value):
+    def activation_function(self, value):
         if value > self.activation_threshold:
             return 1
         return 0
 
     def activate_neuron(self, inputs):
         value = self.feed_forward(inputs)
-        return self.activate_function(value)
+        return self.activation_function(value)
 
     def train(self, inputs):
         attempt = self.activate_neuron(inputs)
         error = self.target - attempt
-        self.weights += inputs * error * learn_const
-        
+        self.weights += inputs * error * self.learn_const
