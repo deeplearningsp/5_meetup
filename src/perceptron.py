9344df96dec2f56af15158eea0abf8a6b883d6c5
Ivan <ispmarin@localhost.localdomain>
2016-09-13 22:53:32 -0300

    add notebook for presentation

--- src/perceptron.py
+++ src/perceptron.py
@@ -3,13 +3,17 @@ import numpy as np
 
 class Perceptron:
     
-    def __init__(self, num_inputs, activation_threshold, target, learn_const):
+    def __init__(self, num_inputs, activation_threshold, learn_const):
         self.num_inputs = num_inputs
         self.activation_threshold = activation_threshold
-        self.target = target
         self.learn_const = learn_const
         self.inputs = np.zeros(self.num_inputs)
         self.weights = np.random.rand(self.num_inputs)
+
+    def __repr__(self):
+        return "len inputs {}, act threshold {}, learn c {}, weights {}".format(
+            self.num_inputs, self.activation_threshold, self.learn_const, self.weights
+        )
 
     def feed_forward(self, inputs):
         return np.sum(inputs * self.weights)
@@ -23,7 +27,7 @@ class Perceptron:
         value = self.feed_forward(inputs)
         return self.activation_function(value)
 
-    def train(self, inputs):
+    def train(self, inputs, target):
         attempt = self.activate_neuron(inputs)
-        error = self.target - attempt
+        error = target - attempt
         self.weights += inputs * error * self.learn_const
